2c32270f0ade2e3e52e418d8f214bcc8685a84de
JonhyNaves <j.naves@campus.fct.unl.pt>
2018-11-26 16:58:12 +0000

    Carros merge com stock 26-11 16.58

--- carrosP.c
+++ carrosP.c
@@ -3,28 +3,47 @@
 #include <strings.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+void lertxt(){
+  struct stock {
+    int id;
+    int amount;
+  };
+  int countF= 0, countAl= 0, countV= 0, countP= 0, countB= 0, countAc= 0;
+  char a;
 
-void lertxt(int qnt[6]){
-  char input[4];
-  int id,quant;
+  struct stock ferro;
+  struct stock aluminio;
+  struct stock vidro;
+  struct stock plastico;
+  struct stock borracha;
+  struct stock aco;
+
+  ferro.id= 1;
+  aluminio.id= 2;
+  vidro.id= 3;
+  plastico.id= 4;
+  borracha.id= 5;
+  aco.id= 6;
+
+  char text[20];
   FILE *fp;
-  fp = fopen("files/quantities.txt","r");
+  fp = fopen("files/stock.txt","r");
   if (fp!=NULL){  
-    while(fgets(input,4,fp)){
-      id = input[0]-48;
-      printf("id:%d\n",id);
-      quant = input[2]-48;
-      printf("quant:%d\n",quant);
-      qnt[id-1] = quant; 
+    while(fgets(text,20,fp)){
+      a = text[0];
+      switch (a){
+        case '1': countF++;break;
+        case '2': countAl++;break;
+        case '3': countV++;break;
+        case '4': countP++;break;
+        case '5': countB++;break;
+        case '6': countAc++;break;
+      }
     }
     fclose(fp);
   }
 } 
 
 void main(){
-  int qnt[6];
-  lertxt(qnt);
-  for(int i=0;i<6;i++){
-    printf("%d\n",qnt[i]);
-  }
+  lertxt();
 }
