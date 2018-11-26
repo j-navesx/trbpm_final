9ef3dc275849f676533ec819f9b9c1c148f3901d
student <gc.pombo@campus.fct.unl.pt>
2018-11-26 07:41:47 -0800

    structs 26-11 15:41

--- view_stock.c
+++ view_stock.c
@@ -4,3 +4,37 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+struct stock {
+
+int id;
+int amount;
+};
+
+void main() {
+
+struct stock ferro;
+struct stock aluminio;
+struct stock vidro;
+struct stock plastico;
+struct stock borracha;
+struct stock aco;
+
+struct ferro.id= 1;
+struct aluminio.id= 2;
+struct vidro.id= 3;
+struct plastico.id= 4;
+struct borracha.id= 5;
+struct aco.id= 6;
+
+char text[20]
+
+
+/*struct stock.price ferro= 1.40;
+struct stock.price aluminio= 7.80;
+struct stock.price vidro= 12.50;
+struct stock.price plastico= 2.20;
+struct stock.price borracha= 1.50;
+struct stock.price aco= 3.30;*/
+
+}
+
