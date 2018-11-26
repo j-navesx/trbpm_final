43198ba9980e1ec540a27b50b49676739cb6c035
student <gc.pombo@campus.fct.unl.pt>
2018-11-26 08:41:37 -0800

    update view_stock e quantities 16:41

--- view_stock.c
+++ view_stock.c
@@ -12,21 +12,23 @@ int amount;
 
 void main() {
 
-struct stock ferro;
-struct stock aluminio;
-struct stock vidro;
-struct stock plastico;
-struct stock borracha;
-struct stock aco;
+  int countF= 0, countAl= 0, countV= 0, countP= 0, countB= 0, countAc= 0;
+  char a;
 
-struct ferro.id= 1;
-struct aluminio.id= 2;
-struct vidro.id= 3;
-struct plastico.id= 4;
-struct borracha.id= 5;
-struct aco.id= 6;
+  struct stock ferro;
+  struct stock aluminio;
+  struct stock vidro;
+  struct stock plastico;
+  struct stock borracha;
+  struct stock aco;
 
-void main(){
+  ferro.id= 1;
+  aluminio.id= 2;
+  vidro.id= 3;
+  plastico.id= 4;
+  borracha.id= 5;
+  aco.id= 6;
+
   char text[20];
   FILE *fp;
   fp = fopen("files/stock.txt","r");
@@ -34,12 +36,29 @@ void main(){
     while(fgets(text,20,fp)){
       a = text[0];
       switch (a){
-        case 1: countF++;
+        case '1': countF++;break;
+        case '2': countAl++;break;
+        case '3': countV++;break;
+        case '4': countP++;break;
+        case '5': countB++;break;
+        case '6': countAc++;break;
       }
       printf("%s",text);
     }
     fclose(fp);
   }
+  
+  printf("%d %d %d %d %d %d\n", countF,countAl,countV,countP,countB,countAc);
+
+  fp= fopen("files/quantities.txt","w+");
+  if(fp!=NULL) {
+    fprintf(fp,"1 %d\n",countF);
+    fprintf(fp,"2 %d\n",countAl);
+    fprintf(fp,"3 %d\n",countV);
+    fprintf(fp,"4 %d\n",countP);
+    fprintf(fp,"5 %d\n",countB);
+    fprintf(fp,"6 %d\n",countAc);
+ }
 
 
 /*struct stock.price ferro= 1.40;
